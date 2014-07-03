548bf959d157f5a93d9a1a2ac86e1d629da06301
phsin <cdr@inbox.ru>
2014-07-03 16:10:23 +0400

    Добавлена возможность декомпилировать ert отчеты 1Сv7

--- pyv8unpack.py
+++ pyv8unpack.py
@@ -81,6 +81,12 @@ def decompile():
     #list of files to decompile and results decompile
     dataprocessor_files = []
 
+    #list of files to decompile and results decompile for 1C v7.7
+    dataprocessor_files_v7 = []
+
+    #list of files to decompile and results decompile for 1C MD
+    dataprocessor_files_MD = []
+
     #set the exit code
     exit_code = 0
 
@@ -92,52 +98,111 @@ def decompile():
             dataprocessor_files.append(filename)
             logging.info("file %s" % filename)
             continue
-    if len(dataprocessor_files) == 0:
-        exit(exit_code)
+       if filename[-3:] == 'ert' :
+            dataprocessor_files_v7.append(filename)
+            logging.info("file %s" % filename)
+            continue            
+       if filename[-2:] == 'md' :
+            dataprocessor_files_MD.append(filename)
+            logging.info("file %s" % filename)
+            continue            
 
     dirsource = os.path.abspath(os.path.join(os.path.curdir, "src"))
-    curabsdirpath = os.path.abspath(os.path.curdir)
-    #pathbin1c = "C:\\Program Files\\1cv82\8.2.17.153\\bin\\1cv8.exe"
-    #pathbin1c = "c:\\Program Files (x86)\\1cv8\\8.3.4.304\\bin\\1cv8.exe"
-    pathbin1c = get_path_to_1c()
+    curabsdirpath = os.path.abspath(os.path.curdir)    
 
-    for filename in dataprocessor_files:
-        print("file %s" % filename)
-        #TODO: добавить копирование этих же файлов в каталог src/имяфайла/...
-        #get file name.
-        fullpathfile = os.path.abspath(filename)
-        basename = os.path.splitext(os.path.basename(filename))[0]
-        fullbasename = os.path.basename(filename)
-        newdirname = os.path.dirname(filename)
+    if len(dataprocessor_files) > 0:
+        #pathbin1c = "C:\\Program Files\\1cv82\8.2.17.153\\bin\\1cv8.exe"
+        #pathbin1c = "c:\\Program Files (x86)\\1cv8\\8.3.4.304\\bin\\1cv8.exe"
+        pathbin1c = get_path_to_1c()
 
-        #Скопируем сначало просто структуру каталогов.
-        if not os.path.exists(dirsource):
-            os.makedirs(dirsource)
-        #для каждого файла определим новую папку.
-        newsourcepath = os.path.join(dirsource, newdirname, basename)
-        if not os.path.exists(newsourcepath):
-            logging.info("create new dir %s" % newsourcepath)
-            os.makedirs(newsourcepath)
+        for filename in dataprocessor_files:
+            print("file %s" % filename)
+            #TODO: добавить копирование этих же файлов в каталог src/имяфайла/...
+            #get file name.
+            fullpathfile = os.path.abspath(filename)
+            basename = os.path.splitext(os.path.basename(filename))[0]
+            fullbasename = os.path.basename(filename)
+            newdirname = os.path.dirname(filename)
 
-        logging.info("file to copy %s, new path %s, new file %s" % (filename, newsourcepath,
-                      os.path.join(newsourcepath,fullbasename)))
+            #Скопируем сначало просто структуру каталогов.
+            if not os.path.exists(dirsource):
+                os.makedirs(dirsource)
+            #для каждого файла определим новую папку.
+            newsourcepath = os.path.join(dirsource, newdirname, basename)
+            if not os.path.exists(newsourcepath):
+                logging.info("create new dir %s" % newsourcepath)
+                os.makedirs(newsourcepath)
 
-        formatstring = format('/C"decompile;pathtocf;%s;pathout;%s;ЗавершитьРаботуПосле;"' % (fullpathfile, newsourcepath))
-        base = '/F"'+os.path.join(curabsdirpath,".git", "hooks","ibService")+'"'
-        V8Reader = '/execute"'+os.path.join(curabsdirpath,".git", "hooks", "V8Reader.epf")+'"'
-        tempbat = tempfile.mktemp(".bat")
-        logging.info("formatstring is %s , base is %s, V8Reader is %s, temp is %s" % (formatstring, base, V8Reader, tempbat))
+            logging.info("file to copy %s, new path %s, new file %s" % (filename, newsourcepath,
+                          os.path.join(newsourcepath,fullbasename)))
 
-        with open(tempbat, 'w', encoding='cp866') as temp:
-            temp.write('@echo off\n')
-            temp.write(format('"%s" %s /DisableStartupMessages %s %s'%(pathbin1c, base, V8Reader, formatstring)))
-            temp.close()
-            result = subprocess.check_call(['cmd.exe', '/C', tempbat])
+            formatstring = format('/C"decompile;pathtocf;%s;pathout;%s;ЗавершитьРаботуПосле;"' % (fullpathfile, newsourcepath))
+            base = '/F"'+os.path.join(curabsdirpath,".git", "hooks","ibService")+'"'
+            V8Reader = '/execute"'+os.path.join(curabsdirpath,".git", "hooks", "V8Reader.epf")+'"'
+            tempbat = tempfile.mktemp(".bat")
+            logging.info("formatstring is %s , base is %s, V8Reader is %s, temp is %s" % (formatstring, base, V8Reader, tempbat))
+
+            with open(tempbat, 'w', encoding='cp866') as temp:
+                temp.write('@echo off\n')
+                temp.write(format('"%s" %s /DisableStartupMessages %s %s'%(pathbin1c, base, V8Reader, formatstring)))
+                temp.close()
+                result = subprocess.check_call(['cmd.exe', '/C', tempbat])
+                result = subprocess.check_call(['git', 'add', '--all', newsourcepath])
+                if not result == 0:
+                    logging.error(result)
+                    exit(result)
+
+    if len(dataprocessor_files_v7) > 0:
+        for filename in dataprocessor_files_v7:
+            print("file %s" % filename)
+            #TODO: добавить копирование этих же файлов в каталог src/имяфайла/...
+            #get file name.
+            fullpathfile = os.path.abspath(filename)
+            basename = os.path.splitext(os.path.basename(filename))[0]
+            fullbasename = os.path.basename(filename)
+            newdirname = os.path.dirname(filename)
+
+            #Скопируем сначало просто структуру каталогов.
+            if not os.path.exists(dirsource):
+                os.makedirs(dirsource)
+            #для каждого файла определим новую папку.
+            newsourcepath = os.path.join(dirsource, newdirname)
+            if not os.path.exists(newsourcepath):
+                logging.info("create new dir %s" % newsourcepath)
+                os.makedirs(newsourcepath)
+
+            t1 = format("gcomp -q -d -F %s -D %s -v --no-ini --no-version --no-empty-mxl" % (filename, newsourcepath))
+            result = subprocess.check_call(['cmd.exe', '/C', t1])
             result = subprocess.check_call(['git', 'add', '--all', newsourcepath])
             if not result == 0:
                 logging.error(result)
                 exit(result)
 
+    if len(dataprocessor_files_MD) > 0:
+        for filename in dataprocessor_files_MD:
+            print("file %s" % filename)
+            #TODO: добавить копирование этих же файлов в каталог src/имяфайла/...
+            #get file name.
+            fullpathfile = os.path.abspath(filename)
+            basename = os.path.splitext(os.path.basename(filename))[0]
+            fullbasename = os.path.basename(filename)
+            newdirname = os.path.dirname(filename)
+
+            #Скопируем сначало просто структуру каталогов.
+            if not os.path.exists(dirsource):
+                os.makedirs(dirsource)
+            #для каждого файла определим новую папку.
+            newsourcepath = os.path.join(dirsource, newdirname, 'MD')
+            if not os.path.exists(newsourcepath):1
+                logging.info("create new dir %s" % newsourcepath)
+                os.makedirs(newsourcepath)
+
+            t1 = format("gcomp -d -v -F %s -D %s -v --no-ini --no-version --no-empty-mxl" % (filename, newsourcepath))
+            result = subprocess.check_call(['cmd.exe', '/C', t1])
+            result = subprocess.check_call(['git', 'add', '--all', newsourcepath])
+            if not result == 0:
+                logging.error(result)
+                exit(result)
 
 if __name__ == '__main__':
     decompile()
