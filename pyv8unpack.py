b49ac9252adc08e77ea7e3ee201471a11639628d
phsin <cdr@inbox.ru>
2018-05-18 09:49:39 +0400

    конвертирование 1251 -> utf8

--- pyv8unpack.py
+++ pyv8unpack.py
@@ -10,7 +10,8 @@ import tempfile
 import re
 import platform
 
-logging.basicConfig(level=logging.ERROR)  # DEBUG => print ALL msgs
+#logging.basicConfig(level=logging.ERROR)  # DEBUG => print ALL msgs
+logging.basicConfig(level=logging.DEBUG)  # DEBUG => print ALL msgs
 
 modified = re.compile('^(?:M|A)(\s+)(?P<name>.*)')
 
@@ -48,6 +49,7 @@ def get_path_to_1c():
     else:
         cmd = subprocess.Popen(["which", "1cv8"], stdout=PIPE).communicate()[0].strip()
     
+    logging.info("CMD: %s" % cmd)
     return cmd 
 
 def get_list_of_comitted_files():
@@ -60,7 +62,7 @@ def get_list_of_comitted_files():
         output = subprocess.check_output(['git','diff-index', '--name-status', '--cached','HEAD']
             ).decode("utf-8")
     except subprocess.CalledProcessError:
-        print("Error diff files get: trace %s" % subprocess.CalledProcessError.output)
+        print("Error diff files get: trace %s" % subprocess.CalledProcessError)
         return files
 
     for result in output.split("\n"):
@@ -77,7 +79,6 @@ def decompile():
     """
     Main functions doing be decompile
     """
-
     #list of files to decompile and results decompile
     dataprocessor_files = []
 
@@ -94,15 +95,11 @@ def decompile():
     for filename in get_list_of_comitted_files():
         #Check the file extensions
         logging.info("file to check %s" % filename)
-        if filename[-3:] in ['epf', 'erf']:
-            dataprocessor_files.append(filename)
-            logging.info("file %s" % filename)
-            continue
         if filename[-3:] == "ert":
             dataprocessor_files_v7.append(filename)
             logging.info("file %s" % filename)
             continue            
-        if filename[-2:] in ['MD','md']:
+        if filename[-3:] in ['.MD','.md']:
             dataprocessor_files_MD.append(filename)
             logging.info("file %s" % filename)
             continue            
@@ -115,43 +112,6 @@ def decompile():
         #pathbin1c = "c:\\Program Files (x86)\\1cv8\\8.3.4.304\\bin\\1cv8.exe"
         pathbin1c = get_path_to_1c()
 
-        for filename in dataprocessor_files:
-            print("file %s" % filename)
-            #TODO: добавить копирование этих же файлов в каталог src/имяфайла/...
-            #get file name.
-            fullpathfile = os.path.abspath(filename)
-            basename = os.path.splitext(os.path.basename(filename))[0]
-            fullbasename = os.path.basename(filename)
-            newdirname = os.path.dirname(filename)
-
-            #Скопируем сначало просто структуру каталогов.
-            if not os.path.exists(dirsource):
-                os.makedirs(dirsource)
-            #для каждого файла определим новую папку.
-            newsourcepath = os.path.join(dirsource, newdirname, basename)
-            if not os.path.exists(newsourcepath):
-                logging.info("create new dir %s" % newsourcepath)
-                os.makedirs(newsourcepath)
-
-            logging.info("file to copy %s, new path %s, new file %s" % (filename, newsourcepath,
-                          os.path.join(newsourcepath,fullbasename)))
-
-            formatstring = format('/C"decompile;pathtocf;%s;pathout;%s;ЗавершитьРаботуПосле;"' % (fullpathfile, newsourcepath))
-            base = '/F"'+os.path.join(curabsdirpath,".git", "hooks","ibService")+'"'
-            V8Reader = '/execute"'+os.path.join(curabsdirpath,".git", "hooks", "V8Reader.epf")+'"'
-            tempbat = tempfile.mktemp(".bat")
-            logging.info("formatstring is %s , base is %s, V8Reader is %s, temp is %s" % (formatstring, base, V8Reader, tempbat))
-
-            with open(tempbat, 'w', encoding='cp866') as temp:
-                temp.write('@echo off\n')
-                temp.write(format('"%s" %s /DisableStartupMessages %s %s'%(pathbin1c, base, V8Reader, formatstring)))
-                temp.close()
-                result = subprocess.check_call(['cmd.exe', '/C', tempbat])
-                result = subprocess.check_call(['git', 'add', '--all', newsourcepath])
-                if not result == 0:
-                    logging.error(result)
-                    exit(result)
-
     if len(dataprocessor_files_v7) > 0:
         for filename in dataprocessor_files_v7:
             print("ert file %s" % filename)
@@ -162,18 +122,33 @@ def decompile():
             fullbasename = os.path.basename(filename)
             newdirname = os.path.dirname(filename)
 
+            print("ert file %s" % fullpathfile )
+
             #Скопируем сначало просто структуру каталогов.
             if not os.path.exists(dirsource):
                 os.makedirs(dirsource)
             #для каждого файла определим новую папку.
             newsourcepath = os.path.join(dirsource, newdirname)
+            newpath2 = os.path.join(newsourcepath, basename)
             if not os.path.exists(newsourcepath):
                 logging.info("create new dir %s" % newsourcepath)
                 os.makedirs(newsourcepath)
+            #print("curabsdirpath %s" % curabsdirpath)
+            #print("newpath2 %s" % newpath2)
+            #print("basename %s" % basename)
 
             t1 = format("gcomp -q -d -F %s -D %s -v --no-ini --no-version --no-empty-mxl" % (filename, newsourcepath))
-            result = subprocess.check_call(['cmd.exe', '/C', t1])
-            result = subprocess.check_call(['git', 'add', '--all', newsourcepath])
+            result = subprocess.check_call(['cmd.exe', '/C', t1])            
+            #изменим кодировку cp1251 на utf-8 
+            #утилита iconv.exe должна запускаться в cmd = добавлена в PATH			
+            #файлов 1s, mdp, frm, txt
+            t3 = 'bash .git/hooks/convert_utf8.sh {0}'.format( newpath2 )
+            print("t3 = %s" % t3)
+            result = subprocess.check_call(['cmd.exe', '/C', t3])
+            #result = subprocess.check_call(['git', 'add', '--all', newsourcepath])
+            result = subprocess.check_call(['git', 'add', '*.1s', newsourcepath])
+            result = subprocess.check_call(['git', 'add', '*.frm', newsourcepath])
+            result = subprocess.check_call(['git', 'add', '*.mxl', newsourcepath])
             if not result == 0:
                 logging.error(result)
                 exit(result)
@@ -187,7 +162,7 @@ def decompile():
             basename = os.path.splitext(os.path.basename(filename))[0]
             fullbasename = os.path.basename(filename)
             newdirname = os.path.dirname(filename)
-
+            
             #Скопируем сначало просто структуру каталогов.
             if not os.path.exists(dirsource):
                 os.makedirs(dirsource)
@@ -196,10 +171,25 @@ def decompile():
             if not os.path.exists(newsourcepath):
                 logging.info("create new dir %s" % newsourcepath)
                 os.makedirs(newsourcepath)
-
+            newpath2 = os.path.join(newsourcepath, basename)
+            print("fullbasename %s" % fullbasename)
+            print("newdirname %s" % newdirname)
+            print("newsourcepath %s" % newsourcepath)
+            
             t1 = format("gcomp -d -v -F %s -D %s" % (filename, newsourcepath))
             result = subprocess.check_call(['cmd.exe', '/C', t1])
-            result = subprocess.check_call(['git', 'add', '--all', newsourcepath])
+
+            #изменим кодировку cp1251 на utf-8 
+            #утилита iconv.exe должна запускаться в cmd = добавлена в PATH			
+            #файлов 1s, mdp, frm, txt
+            t3 = 'bash .git/hooks/convert_utf8.sh {0}'.format( newsourcepath )
+            print("t3 = %s" % t3)
+            result = subprocess.check_call(['cmd.exe', '/C', t3])
+
+            #result = subprocess.check_call(['git', 'add', '--all', newsourcepath])
+            result = subprocess.check_call(['git', 'add', '*.1s', newsourcepath])
+            result = subprocess.check_call(['git', 'add', '*.frm', newsourcepath])
+            result = subprocess.check_call(['git', 'add', '*.mxl', newsourcepath])
             if not result == 0:
                 logging.error(result)
                 exit(result)
