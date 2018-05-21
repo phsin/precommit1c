8b3d7cdf45ec65fded5e39d3064820e7b3a6c9dd
phsin <cdr@inbox.ru>
2018-05-21 23:49:07 +0400

    скрипт ежедневного начала работы

---
+++ _start.bat
@@ -0,0 +1,25 @@
+rem Запускаем в рабочем каталоге
+rem SET work=C:\1C\prioskol\Complex\ExtForms\test\
+SET work=%CD%
+rem 1 записываем все текущие изменения, если что-то не закомичено
+cd %work%
+git add .
+git commit -m "Текущие изменения %DATE%"
+pause
+
+rem 2 получаем все изменения из репозитария
+cd C:\1C\!vetrf_internal
+git pull
+pause
+
+rem 3 копируем в рабочий каталог
+cd C:\1C\!vetrf_internal\ExtForms
+copy *.ert %work%
+rem copy Меркурий_*.ert %work%
+rem copy ВСД_*.ert %work%
+pause 
+
+rem 4 в рабочем каталоге записываем изменения из репозитария
+cd %work%
+git add .
+git commit -m "изменения git %DATE%"
